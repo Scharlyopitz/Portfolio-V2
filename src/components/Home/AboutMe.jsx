a51f02a07cf42de143f457fd38816130640d9b12
Scharly <scharly.opitz@gmail.com>
2024-07-30 13:23:48 +0200

    Changement SVG + mise au propre du code

--- src/components/Home/AboutMe.jsx
+++ src/components/Home/AboutMe.jsx
@@ -6,19 +6,16 @@ import AboutMeSection from "./AboutMeSection";
 import AboutMeBtn from "./AboutMeBtn";
 
 export default function AboutMe() {
-    const [activeBtn, setActiveBtn] = useState(false);
+  const [activeBtn, setActiveBtn] = useState(false);
 
-    return (
-        <section id="aboutMe">
-            <AnimatePresence>
-                {activeBtn && (
-                    <AboutMeSection
-                        activeBtn={activeBtn}
-                        setActiveBtn={setActiveBtn}
-                    />
-                )}
-            </AnimatePresence>
-            <AboutMeBtn activeBtn={activeBtn} setActiveBtn={setActiveBtn} />
-        </section>
-    );
+  return (
+    <section id="aboutMe">
+      <AnimatePresence>
+        {activeBtn && (
+          <AboutMeSection activeBtn={activeBtn} setActiveBtn={setActiveBtn} />
+        )}
+      </AnimatePresence>
+      <AboutMeBtn activeBtn={activeBtn} setActiveBtn={setActiveBtn} />
+    </section>
+  );
 }
--- src/components/Home/AboutMeSection.jsx
+++ src/components/Home/AboutMeSection.jsx
@@ -1,65 +1,116 @@
 import { motion } from "framer-motion";
 
-import {
-    PageAboutAnimation,
-    AnimationSvg,
-    easeInExpo,
-    easeOutCubic,
-} from "../Animations";
+import { PageAboutAnimation, easeInExpo, easeOutCubic } from "../Animations";
 import AboutDescription from "../../Pages/AboutDescription";
+import { useEffect, useRef, useState } from "react";
 
 export default function AboutMeSection({ activeBtn, setActiveBtn }) {
-    return (
-        <>
-            <AboutDescription />
-            <div
-                className="cross-back-container"
-                onClick={() => setActiveBtn(!activeBtn)}
-            >
-                <motion.div
-                    initial={{ y: "110%" }}
-                    animate={{
-                        y: 0,
-                        transition: {
-                            duration: 0.5,
-                            ease: easeOutCubic,
-                            delay: 0.65,
-                        },
-                    }}
-                    exit={{
-                        y: "110%",
-                        transition: { duration: 0.5, ease: easeInExpo },
-                    }}
-                    className="cross-back"
-                ></motion.div>
-            </div>
-            <motion.div
-                initial={{ height: 0 }}
-                animate={{
-                    height: "25%",
-                    transition: {
-                        duration: 0.5,
-                        ease: easeOutCubic,
-                        delay: 0.5,
-                    },
-                }}
-                exit={{
-                    height: 0,
-                    transition: { duration: 0.6, ease: easeInExpo },
-                }}
-                className="trait"
-            ></motion.div>
-            <motion.div
-                variants={PageAboutAnimation}
-                initial="initial"
-                animate="animate"
-                exit="exit"
-                className="sectionAboutMe"
-            >
-                <svg>
-                    <motion.path variants={AnimationSvg} />
-                </svg>
-            </motion.div>
-        </>
-    );
+  return (
+    <>
+      <AboutDescription />
+      <Cross activeBtn={activeBtn} setActiveBtn={setActiveBtn} />
+      <Trait />
+      <PageAbout />
+    </>
+  );
+}
+function Cross({ activeBtn, setActiveBtn }) {
+  return (
+    <div
+      className="cross-back-container"
+      onClick={() => setActiveBtn(!activeBtn)}
+    >
+      <motion.div
+        initial={{ y: "110%" }}
+        animate={{
+          y: 0,
+          transition: {
+            duration: 0.5,
+            ease: easeOutCubic,
+            delay: 0.65,
+          },
+        }}
+        exit={{
+          y: "110%",
+          transition: { duration: 0.5, ease: easeInExpo },
+        }}
+        className="cross-back"
+      ></motion.div>
+    </div>
+  );
+}
+
+function Trait() {
+  return (
+    <motion.div
+      initial={{ height: 0 }}
+      animate={{
+        height: "25%",
+        transition: {
+          duration: 0.5,
+          ease: easeOutCubic,
+          delay: 0.5,
+        },
+      }}
+      exit={{
+        height: 0,
+        transition: { duration: 0.6, ease: easeInExpo },
+      }}
+      className="trait"
+    ></motion.div>
+  );
+}
+
+function PageAbout() {
+  return (
+    <motion.div
+      variants={PageAboutAnimation}
+      initial="initial"
+      animate="animate"
+      exit="exit"
+      className="sectionAboutMe"
+    >
+      <Svg />
+    </motion.div>
+  );
+}
+
+function Svg() {
+  const ref = useRef(null);
+
+  const [height, setHeight] = useState();
+
+  useEffect(() => {
+    setHeight(ref.current.clientHeight);
+  }, []);
+
+  const initialSvgPath = `M0 0 Q ${window.innerWidth / 2} ${height * 2} ${
+    window.innerWidth
+  } 0`;
+
+  const animatedSvgPath = `M0 0 Q ${window.innerWidth / 2} 0 ${
+    window.innerWidth
+  } 0`;
+
+  const AnimationSvg = {
+    initial: { d: initialSvgPath },
+    animate: {
+      d: animatedSvgPath,
+      transition: {
+        duration: 1,
+        ease: [0.65, 0, 0.35, 1],
+      },
+    },
+    exit: {
+      d: initialSvgPath,
+      transition: {
+        duration: 0.6,
+        ease: [0.65, 0, 0.35, 1],
+        delay: 0.3,
+      },
+    },
+  };
+  return (
+    <svg ref={ref}>{height > 0 && <motion.path variants={AnimationSvg} />}</svg>
+  );
 }
